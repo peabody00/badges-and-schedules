33521aa53d04c42ae687cc3accd548af53653e97
Charlie <peabody00@gmail.com>
2020-11-19 19:02:30 -0600

    commit tree a8635310c65a7c726edb638a9054b9f66f9f153a

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,10 @@ def assign_rooms(new_array)
 end
 
 def printer(attendees)
-    puts batch_badge_creator(attendees)
-    puts assign_rooms(attendees)
+    batch_badge_creator(attendees).each do |name|
+        puts name
+    end
+    assign_rooms(attendees).each do |rooms|
+        puts rooms
+    end
 end
